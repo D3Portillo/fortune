ff33066c6e39e724fdb822a11d967ab9dcf490ee
D3Portillo <d3portillo@gmail.com>
2026-01-17 07:04:41 +0100

    chores: init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Fortune Mini App
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos ullam
+      voluptatibus neque in, magni, illo temporibus doloribus cupiditate
+      obcaecati corrupti veniam necessitatibus eius id eaque modi ipsa
+      explicabo? Sapiente, quod?
+    </main>
+  )
+}
